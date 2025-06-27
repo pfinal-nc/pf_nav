4701b76960964e3ad7efb289f13617b6f36a7bf6
pfinal <liuxuzhu@smm.cn>
2025-06-27 18:05:48 +0800

    update

--- apps/web/lib/fonts.ts
+++ apps/web/lib/fonts.ts
@@ -1,11 +1,2 @@
-import { Geist, Geist_Mono } from 'next/font/google'
-
-export const fontSans = Geist({
-  subsets: ['latin'],
-  variable: '--font-sans'
-})
-
-export const fontMono = Geist_Mono({
-  subsets: ['latin'],
-  variable: '--font-mono'
-})
+export const fontSans = '--font-sans';
+export const fontMono = '--font-mono';
