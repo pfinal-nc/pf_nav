05238ac6605de3a7e0c92b960ed9a36c84c89084
pfinal <liuxuzhu@smm.cn>
2025-07-03 13:38:26 +0800

    update

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,12 +2,13 @@
  * @Author: pfinal liuxuzhu@smm.cn
  * @Date: 2025-06-27 16:29:46
  * @LastEditors: pfinal liuxuzhu@smm.cn
- * @LastEditTime: 2025-06-30 09:28:10
+ * @LastEditTime: 2025-07-03 13:38:01
  * @FilePath: /m-nav/apps/web/next.config.mjs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // 添加这一行
   transpilePackages: ["@m-nav/ui"],
   staticPageGenerationTimeout: 300,
   images: {
