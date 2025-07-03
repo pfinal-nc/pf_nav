38120190821681300ef86c56066e56d976ebfaf4
pfinal <liuxuzhu@smm.cn>
2025-07-03 14:36:46 +0800

    update

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,3 @@
-/*
- * @Author: pfinal liuxuzhu@smm.cn
- * @Date: 2025-06-27 16:29:46
- * @LastEditors: pfinal liuxuzhu@smm.cn
- * @LastEditTime: 2025-07-03 14:22:51
- * @FilePath: /m-nav/apps/web/next.config.mjs
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
