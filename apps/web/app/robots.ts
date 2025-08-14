b1869aa95da8601ec3335995ec07d1f3e4b8e531
pfinal <liuxuzhu@smm.cn>
2025-08-14 16:53:14 +0800

    update

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -1,13 +1,18 @@
 import { MetadataRoute } from 'next'
 import { siteConfig } from '@/config/site'
 
+// 添加这行配置
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/private/', '/admin/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
     sitemap: `${siteConfig.url}/sitemap.xml`,
+    host: siteConfig.url,
   }
-} 
+}
