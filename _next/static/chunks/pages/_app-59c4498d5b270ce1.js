26815eae0445a00e79c70d4c7bfe64b776256ad3
pfinal-nc <pfinal-nc@users.noreply.github.com>
2025-12-06 23:10:06 +0000

    deploy: f1fd49aff1ccdb82e108f75f94809b8fd92f4a9e

---
+++ _next/static/chunks/pages/_app-59c4498d5b270ce1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{6582:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(9608)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[774,179],()=>(n(6582),n(9837))),_N_E=_.O()}]);
